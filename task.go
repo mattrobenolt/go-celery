c392f6dac2ce7f6a429fdc41365a45e708c06d2d
Matt Robenolt <matt@ydekproductions.com>
2013-10-26 21:48:59 -0700

    Update Task to handle timestamps from Celery

--- task.go
+++ task.go
@@ -1,9 +1,36 @@
 package celery
 
 import (
+	"bytes"
 	"fmt"
 	"time"
 )
+
+const CELERY_FORMAT = "2006-01-02T15:04:05.999999999"
+
+type celeryTime struct {
+	time.Time
+}
+
+var null = []byte("null")
+
+func (ct *celeryTime) UnmarshalJSON(data []byte) (err error) {
+	if bytes.Equal(data, null) {
+		return
+	}
+	t, err := time.Parse(`"`+CELERY_FORMAT+`"`, string(data))
+	if err == nil {
+		*ct = celeryTime{t}
+	}
+	return
+}
+
+func (ct *celeryTime) MarshalJSON() (data []byte, err error) {
+	if ct.IsZero() {
+		return null, nil
+	}
+	return []byte(ct.Format(`"`+CELERY_FORMAT+`"`)), nil
+}
 
 type Receipt interface {
 	Reply(string, interface{})
@@ -13,14 +40,14 @@ type Receipt interface {
 }
 
 type Task struct {
-	Task string
-	Id string
-	Args []interface{}
-	Kwargs map[string]interface{}
-	Retries int
-	Eta string
-	Expires string
-	Receipt Receipt
+	Task    string                 `json:"task"`
+	Id      string                 `json:"id"`
+	Args    []interface{}          `json:"args"`
+	Kwargs  map[string]interface{} `json:"kwargs"`
+	Retries int                    `json:"retries"`
+	Eta     celeryTime             `json:"eta"`
+	Expires celeryTime             `json:"expires"`
+	Receipt Receipt                `json:"-"`
 }
 
 func (t *Task) Ack(result interface{}) {
